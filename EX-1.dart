63c8732a377e403910fa00251f94e1c5c23aee6f
lida-sokha <sokhalida1234@gmail.com>
2025-10-12 17:03:12 +0700

    finish ex1 and haft ex2

--- EX-1.dart
+++ EX-1.dart
@@ -1,20 +1,75 @@
 enum Skill { FLUTTER, DART, OTHER }
 
 class Employee {
-  String name;
-  double baseSalary;
+  final String _name;
+  double _baseSalary = 40000;
+  final List<Skill> _skills = Skill.values; //to get all enum value into list
+  final List< Map<String, Object>> _address;
+  final int? _yearOfExperience;
 
-  Employee(this.name, this.baseSalary);
+  Employee(
+    this._name,
+    this._baseSalary,
+    List<Skill> skills,
+    this._address,
+    this._yearOfExperience,
+  );
+  //get of private variable
+  String get name => _name;
+  double get baseSalary => _baseSalary;
+  List<Skill> get skills => _skills;
+  List<Map<String, Object>> get address => _address;
+
+  //when the value is nullable
+  int? get yearOfExperience => _yearOfExperience;
 
   void printDetails() {
-    print('Employee: $name, Base Salary: \$${baseSalary}');
+    // print('Employee: $name, Base Salary: \$${baseSalary}');
+    print("Name: $_name");
+    print("BaseSalary: $_baseSalary");
+    print("Year of exerprice: $_yearOfExperience");
+    print("Skill: ${_skills.map((s) => s.name).join(',')}");
+    print("Address: $_address");
+    print("Total salary: ${computeSaraly()}");
   }
+
+  double computeSaraly() {
+    double totalSalary = _baseSalary;
+    if (_yearOfExperience != null) {
+      totalSalary += _yearOfExperience * 2000;
+    }
+    for (var skill in _skills) {
+      switch (skill) {
+        case Skill.FLUTTER:
+          totalSalary += 5000;
+          break;
+
+        case Skill.DART:
+          totalSalary += 3000;
+          break;
+
+        case Skill.OTHER:
+          totalSalary += 1000;
+          break;
+      }
+    }
+    return totalSalary;
+  }
+
 }
 
 void main() {
-  var emp1 = Employee('Sokea', 40000);
+  var emp1 = Employee(
+    'Sokea',
+    40000,
+    [Skill.DART],
+    [
+      {"Street": 4, "City": "Phnom Penh", "Zipcode": 1234},
+    ],
+    3,
+  );
   emp1.printDetails();
 
-  var emp2 = Employee('Ronan', 45000);
+  var emp2 = Employee('Ronan', 45000,[Skill.FLUTTER],[{"Street": 6, "City": "Phnom Penh", "Zipcode": 123450}],4,);
   emp2.printDetails();
 }
--- EX-2.dart
+++ EX-2.dart
@@ -1,31 +1,96 @@
+import 'dart:ffi';
+
 class BankAccount {
-    // TODO
+  // TODO
+  final int _accountID;
+  final String _accountName;
+  final String _accountType;
+  double _balance;
+
+  BankAccount(
+    this._accountID,
+    this._accountName,
+    this._accountType,
+    this._balance,
+  );
+
+  double balance() => _balance;
+
+  double withdraw(double amount) {
+    if (amount <= 0) {
+      print("Invaid amount. Must to greater than 0");
+    } else if (amount > _balance) {
+      throw Exception("Not engough balance to withdraw");
+    } else {
+      _balance -= amount;
+      print("Withdraw: $amount");
+    }
+    return _balance;
+  }
+
+  void credit(double amount) {
+    if (amount > 0) {
+      _balance += amount;
+    } else {
+      print("invait amount");
+    }
+  }
+
+  int get accountID => _accountID;
+  String get accountName => _accountName;
+  String get accountType => _accountType;
 }
 
 class Bank {
-    // TODO
+  // TODO
+  final String bankName;
+  final String location;
+  final List<BankAccount> _accounts = [];
+
+  Bank(this.bankName, this.location);
+
+  BankAccount creatAccount(
+    int accountId,
+    String accountOwner,
+    String accountType,
+    double balance,
+  ) {
+    for (var acc in _accounts) {
+      if (acc._accountID == accountId) {
+        throw Exception("Account with this ID $accountId already exist");
+      }
+    }
+    var newAccount = BankAccount(accountId, accountOwner, accountType, balance);
+    _accounts.add(newAccount);
+    return newAccount;
+  }
 }
- 
+
 void main() {
+  Bank myBank = Bank("CADT Bank", "pp");
+  BankAccount ronanAccount = myBank.creatAccount(1001, 'Ronan', 'credit', 0);
+  // print("Balance: ${ronanAccount.balance()}");
 
-  // Bank myBank = Bank(name: "CADT Bank");
-  // BankAccount ronanAccount = myBank.createAccount(100, 'Ronan');
+  print("Balance: \$${ronanAccount.balance()}"); // Balance: $0
+  ronanAccount.credit(100);
+  print("Balance:\$${ronanAccount.balance()}"); // Balance: $100
+  ronanAccount.withdraw(50);
+  print("${ronanAccount.balance()}"); // Balance: $50
 
-  // print(ronanAccount.balance); // Balance: $0
-  // ronanAccount.credit(100);
-  // print(ronanAccount.balance); // Balance: $100
-  // ronanAccount.withdraw(50);
-  // print(ronanAccount.balance); // Balance: $50
+  try {
+    ronanAccount.withdraw(75); // This will throw an exception
+  } catch (e) {
+    print(e); // Output: Insufficient balance for withdrawal!
+  }
 
-  // try {
-  //   ronanAccount.withdraw(75); // This will throw an exception
-  // } catch (e) {
-  //   print(e); // Output: Insufficient balance for withdrawal!
-  // }
-
-  // try {
-  //   myBank.createAccount(100, 'Honlgy'); // This will throw an exception
-  // } catch (e) {
-  //   print(e); // Output: Account with ID 100 already exists!
-  // }
+  try {
+    myBank.creatAccount(
+      1001,
+      'Honlgy',
+      "savaing",
+      1000,
+    ); // This will throw an exception
+  } catch (e) {
+    print(e); // Output: Account with ID 100 already exists!
+  }
 }
